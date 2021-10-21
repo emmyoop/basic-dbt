397f6351016216197b93777e777f66bc026a26c4
Emily Rockman <emily.rockman@dbtlabs.com>
2021-10-21 14:53:38 -0500

    more rpc testing setup

--- jaffle-shop/rpc_calls/basics.py
+++ jaffle-shop/rpc_calls/basics.py
@@ -4,14 +4,20 @@
 import requests
 import json
 
+DBT_ENV_SECRET_TOKEN = 'some_secret_string'
 
 def main():
     url = "http://localhost:8580/jsonrpc"
 
     payload = {
             "jsonrpc": "2.0",
-            "method": "status",
-            "id": "80949"
+            "method": "poll",
+            "id": "80949",
+            "params": {
+                "request_token": DBT_ENV_SECRET_TOKEN,
+                "logs": True,
+                "logs_start": 0
+            }
           }
     response = requests.post(url, json=payload).json()
 
