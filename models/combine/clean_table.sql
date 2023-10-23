322849f77ca80ac85ec4bfce42dfc16002ddf5bc
Emily Rockman <emily.rockman@dbtlabs.com>
2023-10-23 13:06:39 -0500

    cleanup for incremental model

--- models/combine/clean_table.sql
+++ models/combine/clean_table.sql
@@ -5,8 +5,17 @@
     )
 }}
 
-select first_name || ' ' || last_name  as full_name,
-        email,
-        gender,
-        ip_address
+select id,
+       first_name || ' ' || last_name  as full_name,
+       email,
+       gender,
+       ip_address
     from {{ref('even_id_table')}}
+
+{% if is_incremental() %}
+
+  -- this filter will only be applied on an incremental run
+  -- (uses > to include records filtered by id for manual testing)
+  where id < 200
+
+{% endif %}
